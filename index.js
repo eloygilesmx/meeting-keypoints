04ec7bab1977e8a765e54ddfb01aa8233715e824
Eloy <eloy.giles.og@gmail.com>
2025-01-16 21:17:55 -0600

    Add CORS and test endpoint

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
-// Allow JSON parsing
+// Allow JSON parsing and CORS
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // Basic endpoint that receives POST requests
 app.post('/webhook', (req, res) => {
     console.log('Received webhook:', req.body);
     res.send('Received!');
+});
+
+// Add a basic GET endpoint for testing
+app.get('/', (req, res) => {
+    res.send('Server is running!');
 });
 
 // Start server
